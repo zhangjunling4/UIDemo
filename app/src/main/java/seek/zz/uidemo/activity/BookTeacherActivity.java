c7fd8f882ab45c11077168cf9840ec61c7af04ad
zhangjunling4 <15637181917@163.com>
2017-07-14 16:56:05 +0800

    shape和selector的使用

---
+++ app/src/main/java/seek/zz/uidemo/activity/BookTeacherActivity.java
@@ -0,0 +1,18 @@
+package seek.zz.uidemo.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import seek.zz.uidemo.R;
+
+public class BookTeacherActivity extends AppCompatActivity {
+
+    private TextView tvTotolPrice;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book_teacher);
+    }
+}
---
+++ app/src/main/java/seek/zz/uidemo/activity/MainActivity.java
@@ -0,0 +1,37 @@
+package seek.zz.uidemo.activity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import seek.zz.uidemo.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView info;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.info).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, UserInfoActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        findViewById(R.id.book_teacher).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, BookTeacherActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+}
