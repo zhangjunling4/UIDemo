ab7e5e4f7d82a8dedf19798da4642de1131d083b
zhangjunling4 <15637181917@163.com>
2017-07-17 09:19:16 +0800

    导航栏

---
+++ app/src/main/java/seek/zz/uidemo/navigationbar/NavigationBarActivity.java
@@ -0,0 +1,15 @@
+package seek.zz.uidemo.navigationbar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import seek.zz.uidemo.R;
+
+public class NavigationBarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navigation_bar);
+    }
+}
